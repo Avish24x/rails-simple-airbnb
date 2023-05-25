c4158f7a7ff3dad4af76da775a4c0de552f7fe99
milesh taukoorah <mileshtaukoorah@gmail.com>
2023-05-24 22:31:52 -0700

    rails simple airbnb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :flats
+  resources :flats, only: [:index, :new, :create]
+  resources :flats, except: [:edit]
+
+
 end
